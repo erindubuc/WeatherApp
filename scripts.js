f2db426a6a9d48c79e9ea0605959232391721a49
Erin Dubuc <erin.dubuc9@gmail.com>
2018-08-29 02:17:59 +0000

    working with temp toggle

--- scripts.js
+++ scripts.js
@@ -7,13 +7,15 @@
 /*global showPosition*/
 /*global updateDataToUI*/
 /*global btn*/
+/*global toggleTemp*/
 let loc = document.getElementById("location");
-let temNum = document.getElementById("temperature-num");
+let tempC = document.getElementById("tempC");
+let tempF = document.getElementById("tempF");
 let temScale = document.getElementById("temperature-scale");
 let weatherCon = document.getElementById("weather-condition");
 let weatherIcon = document.getElementById("weather-icon");
-const convert = document.getElementById("convert-temp");
-
+// navigator with geolocation: if it's there, then getWeather
+ // if not, geoloc is not supported
 function getLocation() {
  if (navigator.geolocation) {
    navigator.geolocation.getCurrentPosition(position => {
@@ -24,6 +26,7 @@ function getLocation() {
  }
 }
 // getting weather data from api
+   //I'm still confused about the best practice for writing this... 
 function getWeather(lat, long) {
  const root = "https://fcc-weather-api.glitch.me/api/current?";
  fetch(`${root}lat=${lat}&lon=${long}`, { method: "get" })
@@ -34,124 +37,57 @@ function getWeather(lat, long) {
    .catch(function(err) {
      console.error(err);
    });
-}
-// displaying weather data to DOM
+ }
+// displaying weather data to DOM for user
+// temp displays in celsius, so fahrenheit is not displayed
 function updateDataToUI(location, weather, temp) {
  weatherIcon.innerHTML = `<img src="${weather[0].icon}" />`;
  weatherCon.innerHTML = weather[0].main;
  loc.innerHTML = location;
- temNum.innerHTML = `${temp}`;
-}
-// converting from c to f
+ tempC.innerHTML = `${temp}`+ "°C";
+ tempF.innerHTML = `${temp}` * 9 / 5 + 32 + "°F";
+ tempF.style = "display: none";
+ }
+
+// converting from c to f when f button toggled
 function cToF(celsius) {
-  return celsius * 9 / 5 + 32;
+  var tempF = (tempC * 9 / 5 + 32);
+  return tempF;  
   }
-  // converting from f to c
+  // converting from f to c when c button toggled
  function fToC(fahrenheit) {
-  return (fahrenheit - 32) * 5 / 9;
+  var tempC = (tempF - 32) * 5 / 9;
+  return tempC;
   }
-  
-
-
-
-// getting location when window loads
+ 
+// using navigator.geolocation & getting location when window loads
 window.onload = function() {
  getLocation();
 };
 
-var tempC = Math.round(document.getElementById("temperature-num"));
-var tempF = tempC * 9 / 5 + 32;
-
-
-// switch between celsius and fahrenheit symbols with button
-$(".unitSwitch").click(function(){
-    if ( $(this).text() === '°F' ) { 
-        $('.temperature').html(Math.round((tempF - 32) / 1.8)); // Replaces F with temp converted to C
-        $('.unitSwitch').html('°C')
-    } else  {
-        $('.temperature').html(Math.round(tempC)); // Replaces C temp with original temp by calling the API again
-        $('.unitSwitch').html('°F')
+// switch between celsius and fahrenheit temperatures/letter symbols 
+// function toggleTemp will convert from one scale to the other
+function toggleTempC(){
+ document.getElementById("fahrenheit").onclick;
+  var F = document.getElementById("tempF");
+  var C = document.getElementById("tempC"); 
+    if (C.style.display == "block") {
+        C.style.display = "none";
+        F.style.display = "block";
+    } else {
+        C.style.display = "none";
+        F.style.display = "block";
     }
- });
-
-// need to get temperature to convert when button is clicked
-// //- Using a function pointer:
-// document.getElementById("fahrenheit").click = function cTof() {
-//   btn.addEventListener("click", function cToF() {
-//   document.getElementById("temperature-num").innerHTML = tempF;
-//  });
-//  }
- 
-// document.getElementById("celsius").click = function fToC() {
-//  btn.addEventListener("click", function fToC() {
-//   document.getElementById("temperature-num").innerHTML = tempC;
-//  })
-//  };
-
-
-
-
-
-
-
-
-
-
-
-// // // need to convert celsius to fahrenheit
-// // function cToF(celsius) {
-// //   return celsius * 9 / 5 + 32;
-// //   } 
-// // // convert fahrenheit back to celsius
-// // function fToC(fahrenheit) {
-// //   return (fahrenheit - 32) * 5 / 9;
-// //   }
-//   // now, with a button, you can convert the temps
-// // let btn = document.querySelector('button');
-// //   // event listener to change temperature scales
-// // btn.addEventListener("click", convertTemp()); 
-// // function convertTemp(){
-
-// //   if (btn.innerHTML = "&deg;C") {
-// //     temNum.innerHTML = cToF(celsius).toFixed(2);
-// //     temScale.innerHTML = "&deg;F";
-// //   } else if (btn.innerHTML === "&deg;F") {
-// //     temNum.innerHTML = fToC.toFixed(2);
-// //     temScale.innerHTML = "&deg;C";
-// //   // Get button text
-  
-  
-//   // }
-// // }
-  
-  
-// // toggle celsius to fahrenheit and then back again
-// // function toggleScale() {
-// // if (temScale.innerHTML === "C") {
-// //   temNum.innerHTML = cToF(temNum.innerHTML).toFixed(2);
-// //   temScale.innerHTML = "&deg;F";
-// // } else if (temScale.innerHTML === 'F') {
-// //   temNum.innerHTML = fToC(temNum.innerHTML).toFixed(2);
-// //   temScale.innerHTML = "&deg;C";
-// // }
-
-// // };
-
-
-// //   var btn = $('#convert-temp').text().split('');
-  
-// //   if(btn[1] == 'C'){ //celsius to fahrenheit
-// //     $('#convert-temp').html('&deg;F');
-// //     temNum = (temNum - 32) * .5556;
-// //     return Math.floor(temNum) + '&deg;C';
-// //   }
-// //   else{ //fahrenheit to celsius
-// //     $('#convert-temp').html('&deg;C');
-// //     temNum = temNum * 1.8 + 32
-// //     return Math.floor(temNum) + '&deg;F';
-// //   }
-// // }
-
-// //   $('#convert-temp').on('click', function(){
-// //     $('#temperature-').html(convertTemp());
-// //   });
+}
+function toggleTempF() {    
+ document.getElementById("celsius").onclick;
+  var F = document.getElementById("tempF");
+  var C = document.getElementById("tempC"); 
+    if (F.style.display == "block") {
+        F.style.display = "none";
+        C.style.display = "block";
+    } else {
+        F.style.display = "none";
+        C.style.display = "block";
+    }
+}
